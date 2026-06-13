f32b9eb1cb06238761363b6ce4293a7e29715f8e
Deepak <deepak.siva1981@gmail.com>
2026-06-13 06:34:02 +0530

    New change

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import './App.css'
+import UserParent from './components/parent/UserParent';
 
 function App() {
 
   return (
     <>
-      <h3>Hello Guys</h3>
+      <UserParent/>
     </>
   )
 }
---
+++ src/components/parent/UserParent.tsx
@@ -0,0 +1,9 @@
+export default function UserParent() {
+    return (
+        <>
+            <div className="container">
+                <h3>Parent for all the special users</h3>
+            </div>
+        </>
+    )
+}
