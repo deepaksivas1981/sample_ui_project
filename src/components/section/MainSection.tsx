94aa79454f3ae39f211f3a0221817108d3b6b196
Deepak <deepak.siva1981@gmail.com>
2026-06-16 10:34:36 +0530

    New change

--- src/components/section/MainSection.tsx
+++ src/components/section/MainSection.tsx
@@ -3,7 +3,7 @@ import ActionButton from "../button/ActionButton";
 export default function MainSection() {
     return (
         <>
-            <div className="container">
+            <div className="container mt-3">
                 <ActionButton/>
             </div>
         </>
--- src/components/section/UserParent.tsx
+++ src/components/section/UserParent.tsx
@@ -1,9 +1,10 @@
 export default function UserParent() {
     return (
         <>
-            <div className="container">
-                <h3>Parent for all the special users</h3>
-                <button type="button" className="btn btn-primary">Love Tasks</button>
+            <div className="container mt-3">
+                <h3>Task Manager</h3>
+                <button type="button" className="btn btn-primary">My home grown app for managing my Tasks</button>
+                <hr/>
             </div>
         </>
     )
